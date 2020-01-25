0fd6b0af9fc80f77b6f0024c13978bf40cf6473f
mikelitu <59921026+mikelitu@users.noreply.github.com>
2020-01-25 17:19:16 +0000

    Add files via upload

---
+++ ImageThreshold/ImageThreshold.py
@@ -0,0 +1,99 @@
+import os
+import unittest
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+import logging
+
+class ImageThreshold(ScriptelLoadableModule):
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "Image Threshold"  # TODO make this more human readable by adding spaces
+        self.parent.categories = ["Examples"]
+        self.parent.dependencies = []
+        self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # replace with "Firstname Lastname (Organization)"
+        self.parent.helpText = """
+    This is an example of scripted loadable module bundled in an extension.
+    It performs a simple thresholding on the input volume and optionally captures a screenshot.
+    """
+        self.parent.helpText += self.getDefaultModuleDocumentationLink()
+        self.parent.acknowledgementText = ""
+
+
+class ImageThresholdWidget(ScriptedLoadableModuleWidget):
+
+    def setup(self):
+        ScriptedLoadableModuleWidget.setup(self)
+
+        #
+        #Parameters Area
+        #
+        parametersCollapsibleButton = ctk.ctkCollapsibleButton()
+        parametersCollapsibleButton.text = "Parameters"
+        self.layout.addWidget(parametersCollapsibleButton)
+
+        # Layout within the dummy collapsible button
+
+        parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
+
+        #
+        # input volume selector
+        #
+        self.inputSelector = slicer.qMRMLNodeComboBox()
+        self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.inputSelector.selectNodeUponCreation = True
+        self.inputSelector.addEnabled = False
+        self.inputSelector.removeEnabled = False
+        self.inputSelector.noneEnabled = False
+        self.inputSelector.showHidden = False
+        self.inputSelector.showChildNodeTypes = False
+        self.inputSelector.setMRMLScene(slicer.mrmlScene)
+        self.inputSelector.setToolTip("Pick the input to the algorithm.")
+        parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
+
+        #
+        # output volume selector
+        #
+        self.outputSelector = slicer.qMRMLNodeComboBox()
+        self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.outputSelector.selectNodeUponCreation = True
+        self.outputSelector.addEnabled = True
+        self.outputSelector.removeEnabled = True
+        self.outputSelector.noneEnabled = True
+        self.outputSelector.showHidden = False
+        self.outputSelector.showChildNodeTypes = False
+        self.outputSelector.setMRMLScene(slicer.mrmlScene)
+        self.outputSelector.setToolTip("Pick the output to the algorithm.")
+        parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+
+        #
+        # Apply Button
+        #
+        self.applyButton = qt.QPushButton("Apply")
+        self.applyButton.toolTip = "Run the algorithm."
+        self.applyButton.enabled = False
+        parametersFormLayout.addRow(self.applyButton)
+
+        # connections
+        self.applyButton.connect('clicked(bool)', self.onApplyButton)
+        self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+        self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+
+        # Add vertical spacer
+        self.layout.addStretch(1)
+
+        # Refresh Apply button state
+        self.onSelect()
+
+    def cleanup(self):
+        pass
+
+    def onSelect(self):
+        self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
+
+    def onApplyButton(self):
+        logic = HelloLaplaceLogic()
+        logic.run(self.inputSelector.currentNode(),
+                  self.outputSelector.currentNode())
+        # make the output volume appear in all the slice views
+        slicer.util.setSliceViewerLayers(
+            background=self.outputSelector.currentNode())
